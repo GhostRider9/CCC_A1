8eb0c9397267ea3d04466cbbbba90434ca1de360
GhostRider9 <lpzhang96@qq.com>
2019-04-02 00:06:34 +1100

    Implement function for getting coordinates

--- main.cpp
+++ main.cpp
@@ -4,6 +4,23 @@
 #include <time.h>
 using namespace std;
 
+/*Searching coordinates numbers in raw strings and store them in an array*/
+void getCoordinates(string raw, double *co){
+    regex x("[+-]?(\\d+(\\.\\d+)?|\\.\\d+)([eE][+-]?\\d+)?");
+    auto words_begin =
+            sregex_iterator(raw.begin(), raw.end(), x);
+    auto words_end = sregex_iterator();
+    int j=0;
+    for (sregex_iterator i = words_begin; i != words_end; ++i,++j) {
+        smatch match = *i;
+        co[j]=stod(match.str());
+    }
+}
+
+void getHashtags(){
+
+}
+
 int main() {
     clock_t tStart=clock();
     string twitterData="/home/zlp/data/tinyTwitter.json";
@@ -17,8 +34,11 @@ int main() {
     while (getline(file,eachLine) && i<10){
         regex_search(eachLine,m,hashtags_regex);
         cout<<m.str()<<endl;
+
         regex_search(eachLine,m,geo_regex);
-        cout<<m.str()<<endl;
+        double co[2];
+        getCoordinates(m.str(),co);
+        printf("%.8f,%.8f\n",co[0],co[1]);
         i++;
     }
 
